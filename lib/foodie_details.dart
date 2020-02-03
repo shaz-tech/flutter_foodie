195f4d87e2bc558bf4600db50a76931ae760c34c
Shaz <shahbaz.akhtar@onmobile.com>
2020-02-03 13:01:58 +0530

    Added AnimatedOpacity for the container

--- lib/foodie_details.dart
+++ lib/foodie_details.dart
@@ -17,6 +17,7 @@ class FoodieDetailsState extends State<FoodieDetails> {
   var selectedCard = 'WEIGHT';
   int _itemCartCounter = 0;
   double _totalCart = 0;
+  double _contentOpacity = 0.0;
 
   @override
   void initState() {
@@ -26,6 +27,11 @@ class FoodieDetailsState extends State<FoodieDetails> {
   @override
   Widget build(BuildContext context) {
     _mediaQuerySize = MediaQuery.of(context).size;
+    Future.delayed(const Duration(microseconds: 100), () {
+      setState(() {
+        _contentOpacity = 1.0;
+      });
+    });
 
     return Scaffold(
         backgroundColor: Color(0xFF7A9BEE),
@@ -94,143 +100,149 @@ class FoodieDetailsState extends State<FoodieDetails> {
                 Positioned(
                   top: 256.0,
                   width: _mediaQuerySize.width,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(left: 16.0, right: 16.0),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            widget.food.name,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 24.0,
-                                fontWeight: FontWeight.bold),
+                  child: AnimatedOpacity(
+                    opacity: _contentOpacity,
+                    duration: Duration(seconds: 1),
+                    curve: Curves.easeIn,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              widget.food.name,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 16.0, right: 16.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Text(
-                              "\₹ " + widget.food.price.toString(),
-                              style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 20.0,
-                              ),
-                            ),
-                            Container(
-                              height: 24.0,
-                              width: 1.0,
-                              color: Colors.grey,
-                            ),
-                            Container(
-                              width: 120.0,
-                              height: 40.0,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(
-                                  Radius.circular(16.0),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text(
+                                "\₹ " + widget.food.price.toString(),
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 20.0,
                                 ),
-                                color: Color(0xFF7A9BEE),
                               ),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                  InkWell(
-                                    onTap: () {
-                                      _removeCart();
-                                    },
-                                    child: Icon(
-                                      Icons.remove,
-                                      color: Colors.white,
-                                    ),
+                              Container(
+                                height: 24.0,
+                                width: 1.0,
+                                color: Colors.grey,
+                              ),
+                              Container(
+                                width: 120.0,
+                                height: 40.0,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(16.0),
                                   ),
-                                  Text(
-                                    '$_itemCartCounter',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 16.0),
-                                  ),
-                                  InkWell(
-                                    onTap: () {
-                                      _addCart();
-                                    },
-                                    child: Container(
-                                      height: 24.0,
-                                      width: 24.0,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(8),
+                                  color: Color(0xFF7A9BEE),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    InkWell(
+                                      onTap: () {
+                                        _removeCart();
+                                      },
+                                      child: Icon(
+                                        Icons.remove,
                                         color: Colors.white,
                                       ),
-                                      child: Center(
-                                        child: Icon(
-                                          Icons.add,
-                                          color: Color(0xFF7A9BEE),
+                                    ),
+                                    Text(
+                                      '$_itemCartCounter',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 16.0),
+                                    ),
+                                    InkWell(
+                                      onTap: () {
+                                        _addCart();
+                                      },
+                                      child: Container(
+                                        height: 24.0,
+                                        width: 24.0,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                          color: Colors.white,
+                                        ),
+                                        child: Center(
+                                          child: Icon(
+                                            Icons.add,
+                                            color: Color(0xFF7A9BEE),
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 24.0,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 16.0, right: 16.0),
-                        child: Container(
-                          height: 148.0,
-                          child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            children: <Widget>[
-                              _buildInfoCard('WEIGHT', '300', 'G'),
-                              SizedBox(width: 10.0),
-                              _buildInfoCard('CALORIES', '267', 'CAL'),
-                              SizedBox(width: 10.0),
-                              _buildInfoCard('VITAMINS', 'A, B6', 'VIT'),
-                              SizedBox(width: 10.0),
-                              _buildInfoCard('AVAIL', 'NO', 'AV')
                             ],
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 20.0,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            left: 16.0, right: 16.0, bottom: 16.0),
-                        child: Container(
-                          height: 54.0,
-                          width: _mediaQuerySize.width,
-                          decoration: BoxDecoration(
-                            color: Colors.black,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(8.0),
-                              topRight: Radius.circular(8.0),
-                              bottomLeft: Radius.circular(24.0),
-                              bottomRight: Radius.circular(24.0),
-                            ),
-                          ),
-                          child: Center(
-                            child: Text(
-                              '\₹ $_totalCart',
-                              style: TextStyle(
-                                  fontSize: 20.0, color: Colors.white),
+                        SizedBox(
+                          height: 24.0,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                          child: Container(
+                            height: 148.0,
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: <Widget>[
+                                _buildInfoCard('WEIGHT', '300', 'G'),
+                                SizedBox(width: 10.0),
+                                _buildInfoCard('CALORIES', '267', 'CAL'),
+                                SizedBox(width: 10.0),
+                                _buildInfoCard('VITAMINS', 'A, B6', 'VIT'),
+                                SizedBox(width: 10.0),
+                                _buildInfoCard('AVAIL', 'NO', 'AV')
+                              ],
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                              left: 16.0, right: 16.0, bottom: 16.0),
+                          child: Container(
+                            height: 54.0,
+                            width: _mediaQuerySize.width,
+                            decoration: BoxDecoration(
+                              color: Colors.black,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(8.0),
+                                topRight: Radius.circular(8.0),
+                                bottomLeft: Radius.circular(24.0),
+                                bottomRight: Radius.circular(24.0),
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '\₹ $_totalCart',
+                                style: TextStyle(
+                                    fontSize: 20.0, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
